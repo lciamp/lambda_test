cab56c35ebf1e7b5ca0c7afd1ca8d81407e2d2f2
lciamp <louis.ciampanelli@equinox.com>
2020-01-09 10:06:23 -0500

    iam role

--- lambda.tf
+++ lambda.tf
@@ -13,3 +13,23 @@ resource "aws_lambda_function" "waffle_checkin" {
   runtime          = "python3.7"
   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
 }
+
+resource "aws_iam_role" "iam_for_lambda_tf" {
+  name = "iam_for_lambda_tf"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
